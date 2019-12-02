f6ff08d52e739aa29afbdc1ab95028c8f0fbeabe
torvalds <jian.he@vdaifu.com>
2019-12-02 10:02:28 +0800

    add new data source config and auto injection

---
+++ src/main/java/org/cloud/microservice/business/config/DataSourceConfigForHisUser.java
@@ -0,0 +1,68 @@
+package org.cloud.microservice.business.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * 项目名称：business-spring-boot-starter
+ * 包名称: org.cloud.microservice.business.config
+ * 类描述：hisuser 库的数据源配置文件  datasource.hisuser.enable 是该配置的开关
+ * 创建人：何健
+ *
+ * @author hejian
+ */
+@Configuration
+@ConditionalOnClass({MybatisConfig.class, HikariDataSource.class, SqlSessionFactory.class})
+@ConditionalOnProperty(name = "datasource.hisuser.enable", havingValue = "true", matchIfMissing =
+		true)
+@MapperScan(basePackages = "com.service.business.*.*.dao", sqlSessionTemplateRef =
+		"hisDBSqlSessionTemplate")
+public class DataSourceConfigForHisUser {
+
+	@Value("${ibatis.mapper.locations.hisuser}")
+	private String mapperLocations;
+
+	@Bean(name = "hisDB")
+	@ConfigurationProperties(prefix = "datasource.hisuser")
+	public DataSource fsAppByCateringDataSource() {
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
+	}
+
+	@Bean(name = "hisDBSqlSessionFactory")
+	public SqlSessionFactory fsAppByCateringSqlSessionFactory(@Qualifier("hisDB") DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+		bean.setTypeAliasesPackage(MybatisConfig.getAliases());
+		return bean.getObject();
+	}
+
+	@Bean(name = "hisDBTransactionManager")
+	public DataSourceTransactionManager fsAppByCateringTransactionManager(@Qualifier("hisDB") DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean(name = "hisDBSqlSessionTemplate")
+	public SqlSessionTemplate hisSqlSessionTemplate(
+			@Qualifier("hisDBSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+
+}
+
---
+++ src/main/java/org/cloud/microservice/business/config/DataSourceConfigForPMS.java
@@ -0,0 +1,70 @@
+package org.cloud.microservice.business.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * 项目名称：business-spring-boot-starter
+ * 包名称:org.cloud.microservice.business.config
+ * 类描述：
+ * 创建人：hejian
+ * 创建时间：2019-12-02 08:58
+ * 修改人：hejian
+ * 修改时间：2019-12-02 08:58
+ * 修改备注：
+ *
+ * @author hejian
+ */
+@Configuration
+@ConditionalOnClass({MybatisConfig.class, HikariDataSource.class, SqlSessionFactory.class})
+@ConditionalOnProperty(name = "datasource.pms.enable", havingValue = "true", matchIfMissing = true)
+@MapperScan(basePackages = "com.service.business.*.*.dao", sqlSessionTemplateRef =
+		"pmsDBSqlSessionTemplate")
+public class DataSourceConfigForPMS {
+
+	@Value("${ibatis.mapper.locations.pms}")
+	private String mapperLocations;
+
+	@Bean(name = "pmsDB")
+	@ConfigurationProperties(prefix = "datasource.pms")
+	public DataSource fsAppByCateringDataSource() {
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
+	}
+
+	@Bean(name = "pmsDBSqlSessionFactory")
+	public SqlSessionFactory fsAppByCateringSqlSessionFactory(@Qualifier("pmsDB") DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+		bean.setTypeAliasesPackage(MybatisConfig.getAliases());
+		return bean.getObject();
+	}
+
+	@Bean(name = "pmsDBTransactionManager")
+	public DataSourceTransactionManager fsAppByCateringTransactionManager(@Qualifier("pmsDB") DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean(name = "pmsDBSqlSessionTemplate")
+	public SqlSessionTemplate pmsSqlSessionTemplate(
+			@Qualifier("pmsDBSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+
+}
